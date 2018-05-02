ce9b387cc519c16e3a7b4f51fe42bd32e1f79195
JiaYang <JiaYang627@163.com>
2018-05-02 18:22:02 +0800

    test

--- app/src/main/java/com/jiayang/mvp/mvpframework/p/MainActivityPst.java
+++ app/src/main/java/com/jiayang/mvp/mvpframework/p/MainActivityPst.java
@@ -32,6 +32,7 @@ public class MainActivityPst extends BasePresenter<ImainAcitivityView> {
 //        测试跳转 TestActivity 时打开即可
         wyNavigate.goToTestActivity(context);
         LogUtils.e("zk----Test");
+        LogUtils.e("zk----Test");
 
         // ↑ 和 ↓ 两个必须 只存一个！！！！ 测试其中一个 另外一个必须注释掉
 
